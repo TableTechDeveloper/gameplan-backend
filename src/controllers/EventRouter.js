48d64fd0f3c64e4a17eb34ba7fc7eac026069931
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-07-22 21:42:26 +1000

    feat: implement single event route, fixed minor code errors

--- src/controllers/EventRouter.js
+++ src/controllers/EventRouter.js
@@ -2,18 +2,41 @@ const express = require("express");
 const { Event } = require("../models/models");
 const router = express.Router();
 
-// Route to GET and display all events
+// Route to GET and display all PUBLIC and PUBLISHED events
 router.get("/", async (request, response, next) => {
     try {
-        let foundEvents = await Event.find({}).exec()
-        response.status(200).json(foundEvents)
+        let foundEvents = await Event.find({isPublic: true, isPublished: true}).exec();
+        response.status(200).json(foundEvents);
     } catch (error) {
-        next({
+        response.status(500).json({
             status: 500,
             message: "Error retrieving events",
             errors: [error.message]
-        })
+        });
     }
-})
+});
 
-module.exports = router
+// Route to GET and display event when given id
+router.get("/:id", async (request, response, next) => {
+    try {
+        let result = await Event.findById(request.params.id).exec();
+        if (!result) {
+            return response.status(404).json({
+                status: 404,
+                message: "Event not found",
+                errors: ["This event does not exist"]
+            });
+        }
+        response.json(result);
+    } catch (error) {
+        response.status(500).json({
+            status: 500,
+            message: "Error retrieving event",
+            errors: [error.message]
+        });
+    }
+});
+
+
+
+module.exports = router;
