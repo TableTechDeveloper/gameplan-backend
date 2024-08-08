f38be8768559f51b5913ddc493857b5b79669bb2
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-08-08 21:00:19 +1000

    feat: additional jest testing
    73.21% coverage @56 tests

--- test/userTesting.test.js
+++ test/userTesting.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../src/server")
 const mongoose = require("mongoose");
 const { databaseURL } = require("../src/config/config");
-const { User, Game } = require("../src/models/models");
+const { User } = require("../src/models/models");
 const { createJWT } = require("../src/utils/_utils");
 
 let token = ""
@@ -12,11 +12,14 @@ let badToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2YjQ0NWE0N2JjZTd
 
 beforeAll(async () => {
     await mongoose.connect(databaseURL);
-        await User.deleteMany({})
+        await User.deleteMany({
+            email: { $in: ["PrimaryUser@test.com", "DeletedUser@test.com", "WrongUser@test.com", "jest@test.com"]}
+        })
+
         const user = new User ({
-            email: "ADMIN@test.com",
+            email: "PrimaryUser@test.com",
             password: "authenticated@1234",
-            username: "ADMIN",
+            username: "PrimaryUser",
             securityQuestionOne: "One",
             securityQuestionTwo: "Two",
             securityQuestionThree: "Three"
@@ -25,9 +28,9 @@ beforeAll(async () => {
         await user.save();
         token = createJWT(user._id)
         const userToBeDeleted = new User ({
-            email: "ADMIN2@test.com",
+            email: "DeletedUser@test.com",
             password: "authenticated@1234",
-            username: "ADMIN2",
+            username: "DeletedUser",
             securityQuestionOne: "One",
             securityQuestionTwo: "Two",
             securityQuestionThree: "Three"
@@ -37,9 +40,9 @@ beforeAll(async () => {
         deletedUserToken = createJWT(userToBeDeleted._id);
 
         const wrongUser = new User ({
-            email: "ADMIN3@test.com",
+            email: "WrongUser@test.com",
             password: "authenticated@1234",
-            username: "ADMIN3",
+            username: "WrongUser",
             securityQuestionOne: "One",
             securityQuestionTwo: "Two",
             securityQuestionThree: "Three"
@@ -50,11 +53,10 @@ beforeAll(async () => {
 });
 
 afterAll(async() => {
-    await mongoose.connection.db.dropDatabase();
     await mongoose.connection.close();
 })
-// Test UserRoutes
-describe('UserRouter-standard', () => {
+
+describe('UserRouter', () => {
     describe("POST /user/register", () => {
         it("should register a new user", async () => {
             const user = {
@@ -307,143 +309,6 @@ describe('UserRouter-standard', () => {
         });
     });
 
-});
-// Test GameRoutes
-describe("GameRouter", () => {
-    describe("GET /games/search", () => {
-        it("should return with a single boardgame", async () => {
-
-            const response = await request(app).get('/games/search')
-            .set("Authorization", `Bearer ${token}`)
-            .query({
-                query: "Galaxy Trucker",
-                strict: "true"
-            })
-
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty("message", "Games retrieved successfully")
-        });
-
-        it("should return with multiple boardgames", async () => {
-
-            const response = await request(app).get('/games/search')
-            .set("Authorization", `Bearer ${token}`)
-            .query({
-                query: "Galaxy",
-                strict: "false"
-            })
-
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty("message", "Games retrieved successfully")
-        });
-
-        it("should return an error when search parameter missing", async () => {
-
-            const response = await request(app).get('/games/search')
-            .set("Authorization", `Bearer ${token}`)
-            .query({
-                strict: "false"
-            })
-
-            expect(response.status).toBe(400)
-            expect(response.body).toHaveProperty("message", "Missing search query")
-        });
-
-        it("should return with an error when no game is found", async () => {
-
-            const response = await request(app).get('/games/search')
-            .set("Authorization", `Bearer ${token}`)
-            .query({
-                query: "GalaxyTrucker",
-                strict: "true"
-            })
-
-            expect(response.status).toBe(404)
-            expect(response.body).toHaveProperty("message", "No games found")
-        });
-
-    })
-
-    describe("GET /games/:id", () => {
-        it("should return with details about a single boardgame", async () => {
-
-            const response = await request(app).get('/games/31481')
-            .set("Authorization", `Bearer ${token}`)
-
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty("message", "Game retrieved successfully")
-        });
-
-        it("should return with an error if the game does not exist", async () => {
-
-            const response = await request(app).get('/games/999999')
-            .set("Authorization", `Bearer ${token}`)
-
-            expect(response.status).toBe(404)
-            expect(response.body).toHaveProperty("message", "Game not found")
-        });
-
-    })
-
-    describe("POST /add", () => {
-        it("should add a boardgame to the user", async () => {
-            const game = { gameId: "31481" }
-
-            const response = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send(game);
-
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty("message", "Galaxy Trucker added to collection successfully")
-        });
-
-        it("should error when no gameId is provided", async () => {
-            const game = {}
-
-            const response = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send(game);
-
-            expect(response.status).toBe(400)
-            expect(response.body).toHaveProperty("message", "Missing game ID")
-        });
-
-        it("should error when no game is found", async () => {
-            const game = { gameId: "999999" }
-
-            const response = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send(game);
-
-            expect(response.status).toBe(404)
-            expect(response.body).toHaveProperty("message", "Game not found")
-        });
-
-        it("should error when user is not authenticated", async () => {
-            const game = { gameId: "31481" }
-
-            const response = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${badToken}`)
-            .send(game);
-
-            expect(response.status).toBe(404)
-            expect(response.body).toHaveProperty("message", "User not found")
-        });
-
-        it("should error when user attempts to add same game twice", async () => {
-            const game = { gameId: "31481" }
-
-            const response = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send(game);
-
-            expect(response.status).toBe(400)
-            expect(response.body).toHaveProperty("message", "Game already in collection")
-        });
-    })
-})
-// Test UserRoutes after games have been populated
-describe("UserRouter-Games", () => {
     describe("GET /user/collection", () => {
         it("should show all games added to the users collection", async () => {
             const response = await request(app).get("/user/collection")
@@ -471,172 +336,6 @@ describe("UserRouter-Games", () => {
             expect(response.status).toBe(404)
             expect(response.body).toHaveProperty("message", "User not found")
         });
-
-
     })
 
-    describe("DELETE /user/collection/:id", () => {
-        it("should delete a game from the users collection", async () => {
-            await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send("31481");
-
-            const gameToRemove = await Game.findOne({ boardgamegeekref: 31481 })
-
-            const response = await request(app).delete(`/user/collection/${gameToRemove._id}`)
-            .set("Authorization", `Bearer ${token}`)
-
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty("message", "Game: Galaxy Trucker has been removed from ADMIN's collection successfully")
-        });
-
-        it("should error if the game is not found", async () => {
-            const user = await User.findOne({ email: "ADMIN@test.com" }).exec()
-
-            const response = await request(app).delete(`/user/collection/${user._id}`)
-            .set("Authorization", `Bearer ${token}`)
-
-            expect(response.status).toBe(404)
-            expect(response.body).toHaveProperty("message", "Game not found")
-        });
-
-        it("should error if the user is not found", async () => {
-            await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send("426129");
-
-            const gameToRemove = await Game.findOne({ boardgamegeekref: 31481 })
-
-            const response = await request(app).delete(`/user/collection/${gameToRemove._id}`)
-            .set("Authorization", `Bearer ${badToken}`)
-
-            expect(response.status).toBe(404)
-            expect(response.body).toHaveProperty("message", "User not found")
-        });
-
-        it("should error if the game is not in the users game collection", async () => {
-            const gameToRemove = await Game.findOne({ boardgamegeekref: 31481 })
-
-            const response = await request(app).delete(`/user/collection/${gameToRemove._id}`)
-            .set("Authorization", `Bearer ${token}`)
-
-            expect(response.status).toBe(400)
-            expect(response.body).toHaveProperty("message", "Game not in collection")
-        });
-    })
-})
-
-describe("EventRouter", () => {
-    describe("GET /events/", () => {
-        it("should show all public and published events", async () => {
-            const response = await request(app).get("/events/");
-
-            expect(response.status).toBe(200)
-            expect(response.body).toHaveProperty("message", "Events retrieved successfully")
-        });
-    });
-
-    describe("POST /events/new", () => {
-        it("should create a new public draft event", async () => {
-            // Add game to user collection
-            const game = { gameId: "31481" }
-            const addedGame  = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${token}`)
-            .send(game);
-
-            const event = {
-                title: "Test event",
-                game: addedGame.body.id,
-                isPublished: false
-            };
-
-            const response = await request(app).post("/events/new")
-            .set("Authorization", `Bearer ${token}`)
-            .send(event);
-
-            expect(response.status).toBe(201);
-            expect(response.body).toHaveProperty("message", "Event created successfully")
-        });
-
-        it("should create a new public published event", async () => {
-            // Add game to user collection
-            const foundGame = await Game.findOne({ boardgamegeekref: 31481 }).exec()
-
-            const event = {
-                title: "Test published event",
-                eventDate: new Date(),
-                game: foundGame._id,
-                location: "My house",
-                isPublished: true,
-                isPublic: true
-            };
-
-            const response = await request(app).post("/events/new")
-            .set("Authorization", `Bearer ${token}`)
-            .send(event);
-
-            expect(response.status).toBe(201);
-            expect(response.body).toHaveProperty("message", "Event created successfully")
-        });
-
-        it("should return an error if the game does not exist", async () => {
-            const notRealGame = await User.findOne({}).exec()
-            const event = {
-                title: "Test event",
-                game: notRealGame._id,
-                isPublished: false
-            };
-
-            const response = await request(app).post("/events/new")
-            .set("Authorization", `Bearer ${token}`)
-            .send(event);
-
-            expect(response.status).toBe(400);
-            expect(response.body).toHaveProperty("message", "Game not found")
-        });
-
-        it("should return an error if the host does not own the game for the event", async () => {
-            const game = { gameId: "179172" }
-            const notOwnedGame = await request(app).post('/games/add')
-            .set("Authorization", `Bearer ${wrongUserToken}`)
-            .send(game);
-
-            console.log("GAME ID: ", notOwnedGame.body.id)
-
-            const event = {
-                title: "Test event",
-                game: notOwnedGame.body.id,
-                isPublished: false
-            };
-            console.log(event.game)
-
-            const response = await request(app).post("/events/new")
-            .set("Authorization", `Bearer ${token}`)
-            .send(event);
-
-            expect(response.status).toBe(400);
-            expect(response.body).toHaveProperty("message", "Game not owned")
-        });
-
-        it("should return an error if there is missing details for a published game", async () => {
-            // Add game to user collection
-            const foundGame = await Game.findOne({ boardgamegeekref: 31481 }).exec()
-
-            const event = {
-                title: "Test published event",
-                eventDate: new Date(),
-                game: foundGame._id,
-                isPublished: true,
-                isPublic: true
-            };
-
-            const response = await request(app).post("/events/new")
-            .set("Authorization", `Bearer ${token}`)
-            .send(event);
-
-            expect(response.status).toBe(400);
-            expect(response.body).toHaveProperty("message", "Missing required fields for published event")
-        });
-
-    })
-})
+});
